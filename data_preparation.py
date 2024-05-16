9abde7296ad6f4737d2e460484aa7d6d3f161d0e
Meron <merronmuche@gmail.com>
2024-05-16 22:27:22 +0300

    working for batch and generator

--- data_preparation.py
+++ data_preparation.py
@@ -25,8 +25,23 @@ class_dict = {
     '90': 15
 }
 
+batch_size = 32
+total_data = 2668
 
-def gen_series(path):
+steps_per_epoch = total_data//batch_size
+
+def load_and_preprocess_image(image_path, problematic_images):
+    try:
+        with Image.open(image_path) as image:
+            image = image.resize((299, 299)).convert('RGB')  # Resize to 299x299 and convert to RGB
+            img = np.array(image) / 255.0
+        return img
+    except (OSError, IOError, ValueError) as e:
+        print(f"Error loading or processing image '{image_path}': {e}")
+        problematic_images.append(image_path)  
+        return None
+
+def gen_series(path, batch_size=2, shuffle = True):
     try:
         path = path.decode('utf-8')  # Assuming 'utf-8' encoding
     except:
@@ -41,112 +56,35 @@ def gen_series(path):
         files = list(clas.iterdir())
         image_paths += files
     
-    for image_path in image_paths:
-        # get the label 
-        label_key = image_path._cparts[2]
-        label = class_dict[label_key]
-
-        image = Image.open(image_path)
-        image = image.resize((299, 299)).convert('RGB')  # Resize to 299x299 and convert to RGB
-
-        image = image.resize(size=(299,299))
-        img = np.array(image)/255
-
-        # you do the image transformations
-        yield img, label
-
-# y  = gen_series('data')
-
-# for i in y:
-#     print(i)
-
-
-x_train = tf.data.Dataset.from_generator(
-    gen_series,
-    args=['data'],
-    output_signature=(
-            tf.TensorSpec(shape=(299, 299, 3), dtype=tf.float32),  # Image tensor
-            tf.TensorSpec(shape=(), dtype=tf.int32)  # Label tensor
-        )   
-)
-
-x_train = x_train.shuffle(20).padded_batch(32)
-
-# ids, sequence_batch = next(iter(x_train))
-# print(ids.numpy())
-# print()
-# print(sequence_batch.numpy())
-
-# for imgs, lbls in x_train:
-#         print(f"Label: {lbls}")
-
-
-def load_and_preprocess_image(image_path, problematic_images):
-    try:
-        with Image.open(image_path) as image:
-            image = image.resize((299, 299)).convert('RGB')  # Resize to 299x299 and convert to RGB
-            img = np.array(image) / 255.0
-        return img
-    except (OSError, IOError, ValueError) as e:
-        print(f"Error loading or processing image '{image_path}': {e}")
-        problematic_images.append(image_path)  
-        return None
-
-
-# x_test
-def gen_series(path):
-    try:
-        path = path.decode('utf-8')  
-    except:
+    # shuffle the image paths list
+    if shuffle:
         pass
-    path = Path(path)
-    path_train = path / 'test'
+    num_data = len(image_paths)
+    print(f'the number of images is {num_data}')
+
+    n_batches = num_data//batch_size
     
-    classes = list(path_train.iterdir())
-    class_dict = {str(clas).split('/')[-1]: i for i, clas in enumerate(classes)}
+    for batch in range(n_batches):
+        imgs = []
+        lbls = []
+        for i in range(batch*batch_size, batch*batch_size+batch_size):
+            image_path = image_paths[i]
+            # get the label 
+            label_key = image_path._cparts[2]
+            label = class_dict[label_key]
 
-    image_paths = []
-    for clas in classes:
-        files = list(clas.iterdir())
-        image_paths += files
-    
-    problematic_images = [] 
-    
-    for image_path in image_paths:
-        # Get the label
-        label_key = image_path.parts[-2]
-        label = class_dict.get(label_key, -1)  
+            image = Image.open(image_path)
+            image = image.resize((299, 299)).convert('RGB')  # Resize to 299x299 and convert to RGB
+            # image = image.convert('RGB')
+            img = np.array(image)/255
+            imgs.append(img)
+            lbls.append(label)
+        
+        imgs = np.array(imgs)
+        lbls = np.array(lbls)
+        yield imgs, lbls
 
-        # Load and preprocess the image
-        img = load_and_preprocess_image(image_path, problematic_images)
-        if img is not None:
-            yield img, label
-
-    return problematic_images 
-
-
-# Create a TensorFlow dataset from the generator function
-x_test = tf.data.Dataset.from_generator(
-    gen_series,
-    args=['data'],
-    output_signature=(
-        tf.TensorSpec(shape=(299, 299, 3), dtype=tf.float32),  # Image tensor
-        tf.TensorSpec(shape=(), dtype=tf.int32)  # Label tensor
-    )
-)
-
-# # Test the dataset by iterating through it
-# for img, lbl in x_test.take(10):  
-#     print(f"Label: {lbl}")
-
-# # Retrieve the list of problematic image paths after processing all images
-# problematic_images = next(iter(x_test))
-
-# # Review the problematic images and decide which ones to delete
-# for image_path in problematic_images:
-#     print(f"Problematic image: {image_path}")
-
-
+trrain_data = gen_series('data') 
 
 
 model = keras.applications.InceptionV3(include_top=True)
@@ -163,5 +101,8 @@ new_model.compile(
 )
 
 # print(new_model.summary())
-new_model.fit(x_train, epochs=3, verbose=2)
+new_model.fit(trrain_data, epochs=3, steps_per_epoch=steps_per_epoch)
+
+# new_model.evaluate(trrain_data, epochs=3, verbose=2)
+
 
